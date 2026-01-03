09029aaf5b395e2893ee5547b385717815629fc5
klisarsana <muchliswicaksana.ar@gmail.com>
2026-01-04 00:29:22 +0700

    Update footer and json
    json : react version

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="border-t bg-muted/20 py-12">
-      <div className="wrapper">BuiltSpace Inc. All rights reserved</div>
+      <div className="wrapper text-center text-muted-foreground">BuiltSpace Inc. All rights reserved</div>
     </footer>
   );
 };
